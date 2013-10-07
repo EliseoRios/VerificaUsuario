76a91b11bc92a196bba7f40be6fb743920be0a8f
EliseoRios <eliseohack@gmail.com>
2013-10-07 12:59:47 -0500

    problema corregido...

--- Capturando.php
+++ Capturando.php
@@ -3,35 +3,12 @@
   <title>Usuario</title>
  </head>
  <body>
-  <form method="post">
+  <form method="post" action="Iniciar.php">
    C&oacutedigo: <input type="text" name="codigo"/>
    <br/>
    Contrase&ntildea: <input type="password" name="contrasenia"/>
    <br/>
-   <input type="submit" name="Entrar"/>
-   
-   <?php
-   $codigo=$_POST['codigo'];
-   $contrasenia=$_POST['contrasenia'];
-   if(trim($codigo)!=null and trim($contrasenia)!=null)
-   {
-    if($codigo=="100" && $contrasenia=="udg")
-    {
-     header('Location:Iniciar.php');
-    }
-    else
-    {
-     header('Location:Incorrecto.php');
-    }
-   }
-   else
-   {
-    echo "<h4>Favor de llenar los espacios</h4>";
-   }
-
-   unset($_POST['codigo'] );
-   unset($_POST['contrasenia'] );
-   ?>
+   <input type="submit" name="Entrar" value="Enviar" onSubmit="true"/>
   </form>
  </body>
 </html>
--- Iniciar.php
+++ Iniciar.php
@@ -4,10 +4,25 @@
  </head>
  
  <body>
-  <h1>HAS INICIADO SESION EXITOSAMENTE<h1>
-  
   <form method="post" action="Capturando.php">
-   <input type="submit" value="Cerrar Sesion"/>
+   <input type="submit" value="Cerrar"/>
   </form>
+  
+   <?php
+   $codigo=$_POST['codigo'];
+   $contrasenia=$_POST['contrasenia'];
+   
+    if($codigo=="100" && $contrasenia=="udg")
+    {   
+	 echo   "<h1>DATOS CORRECTOS</h1><p>Has accedido con c&oacutedigo: ".$codigo."</p>";
+    }
+    else
+    {
+	 echo   "<h1>DATOS INCORRECTOS<h1>";
+    }
+
+    unset($_POST['codigo'] );
+    unset($_POST['contrasenia'] );
+   ?>
  </body>
 </html>
